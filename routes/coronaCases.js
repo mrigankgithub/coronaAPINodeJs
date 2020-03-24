b0f8b6d1ba3a85c6980462029f52da4ca7b69908
mrigankgithub <mrigank.taxak@appinventiv.com>
2020-03-25 01:17:09 +0530

    router changed

--- routes/coronaCases.js
+++ routes/coronaCases.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const dataController  = require('../controllers/coronaCases');
 
-router.get('/list', dataController.getCorona)
+router.get('', dataController.getCorona)
 
 module.exports = router;
